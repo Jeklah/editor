b8a1687241bdeb16908f916e20cbe5034682e421
Arthur Bowers <arthur.bowers@phabrix.com>
2025-05-22 15:18:15 +0100

    Adding main.rs and editor.rs and initial commit.

---
+++ src/editor.rs
@@ -0,0 +1,32 @@
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
+use std::io::{self, Read};
+
+pub struct Editor {
+    // Add any necessary fields here
+}
+
+impl Editor {
+    pub fn default() -> Self {
+        Editor {}
+    }
+    pub fn run(&self) {
+        enable_raw_mode().unwrap();
+        for b in io::stdin().bytes() {
+            match b {
+                Ok(b) => {
+                    let c = b as char;
+                    if c.is_control() {
+                        println!("Binary: {b:08b} ASCII: {b:#03}\r");
+                    } else {
+                        println!("Binary: {b:08b} ASCII: {b:#03} Character: {c:#?}\r");
+                    }
+                    if c == 'q' {
+                        break;
+                    }
+                }
+                Err(err) => println!("Error: {err}"),
+            }
+        }
+        disable_raw_mode().unwrap();
+    }
+}
