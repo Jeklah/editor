db5ba66fbc477e75e2d8dce0467e86b582e1be57
Arthur Bowers <arthur.bowers@phabrix.com>
2025-05-23 16:11:26 +0100

    Adding a mess of editor.rs update. W.I.P

--- src/editor.rs
+++ src/editor.rs
@@ -1,30 +1,61 @@
-use crossterm::event::{read, Event::Key, KeyCode::Char};
-use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
+use crossterm::event::{read, Event, Event::Key, KeyCode::Char, KeyEvent, KeyModifiers};
+use crossterm::execute;
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode, Clear, ClearType};
+use std::io::stdout;
+mod terminal;
 
 pub struct Editor {
-    // Add any necessary fields here
+    should_quit: bool,
+    Editor { should_quit: false }
+}
+pub fn run(&mut self) {
+    Self::initialize().unwrap();
+    let result = self.repl();
+    Self::terminate().unwrap();    result.unwrap();
 }
 
-impl Editor {
-    pub fn default() -> Self {
-        Editor {}
-    }
-    pub fn run(&self) {
-        enable_raw_mode().unwrap();
-        loop {
-            match read() {
-                Ok(Key(event)) => {
-                    print!("{event:?} \r");
-                    if let Char(c) = event.code {
-                        if c == 'q' {
-                            break;
-                        }
-                    }
-                }
-                Err(err) => println!("Error: {err}"),
-                _ => (),
-            }
+fn initialize() -> crossterm::Result<(), std::io::Error> {
+    enable_raw_mode()?;
+    Self::clear_screen()
+}
+fn terminate() -> crossterm::Result<(), std::io::Error> {
+    disable_raw_mode()
+}
+fn clear_screen() -> Result<(), std::io::Error> {
+    let mut stdout = stdout();
+    execute!(stdout, Clear(ClearType::All))
+}
+
+fn repl(&mut self) -> Result<(), std::io::Error> {
+    loop {
+        let event = read()?;
+        self.evaluate_event(&event)?;
+        self.refresh_screen()?;
+        if self.should_quit {
+            break;
         }
-        disable_raw_mode().unwrap();
     }
+    Ok(())
+}
+
+fn evaluate_event(&mut self, event: &Event) {
+    if let Key(KeyEvent {
+        code, modifiers, ..
+    }) = event
+    {
+        match code {
+            Char('q') if *modifiers == KeyModifiers::CONTROL => {
+                self.should_quit = true;
+            }
+            _ => (),
+        }
+    }
+}
+
+fn refresh_screen(&sefl) -> Result<(), std::io::Error> {
+    if self.should_quit {
+        Self::clear_screen()?;
+        print!("Goodbye!\r\n");
+    }
+    Ok(())
 }
